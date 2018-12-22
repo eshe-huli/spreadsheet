0cd1196a334f909731c60d324901f3d915dd07bf
Ben Kuhn <ben.s.kuhn@gmail.com>
2018-12-22 16:07:09 -0500

    Extremely janky handwritten recursive descent parser

--- ss/formula.py
+++ ss/formula.py
@@ -1,49 +1,124 @@
 import ast
+import re
+import enum
+from typing import NamedTuple
 
 def parse(formula):
-    """Parse a formula like `=A1+2+ABS(A2)`.
+    tokens = tokenize(formula)
+    return Parser(tokens).parse()
 
-    Returns a set of nested lists:
+# If you add a group here, add it to TokenType below also
+LEXER = re.compile(r"""
+(?P<lparen>\()
+| (?P<rparen>\))
+| (?P<value>[a-zA-Z0-9:-]+)
+| (?P<plus>\+)
+| (?P<minus>-)
+| (?P<times>\*)
+| (?P<divided>/)
+| (?P<comma>,)
+| (?P<whitespace>\s+)
+""", re.VERBOSE)
 
-    - The first element of the list is the name of the function or binary
-      operation to apply
-    - Each subsequent element is a string (= cell reference) or an integer (=
-      a literal int)
-    """
-    pyast = ast.parse(formula, mode='eval')
-    body = pyast.body
-    return _pyast_to_myast(body)
+class TokenType(enum.Enum):
+    LPAREN = enum.auto()
+    RPAREN = enum.auto()
+    VALUE = enum.auto()
+    PLUS = enum.auto()
+    MINUS = enum.auto()
+    TIMES = enum.auto()
+    DIVIDED = enum.auto()
+    COMMA = enum.auto()
+    EOF = enum.auto()
+
+class Token(NamedTuple):
+    type: TokenType
+    value: str
+
+def tokenize(text):
+    last = 0
+    for match in LEXER.finditer(text):
+        start, end = match.span()
+        if start != last:
+            bad_token = text[last:start]
+            raise ParseError(f"Unknown token {bad_token!r}")
+        last = end
+        values = [
+            (k, v)
+            for k, v in match.groupdict().items()
+            if v
+        ]
+        assert len(values) == 1
+        (groupname, value) = values[0]
+        if groupname != 'whitespace':
+            yield Token(getattr(TokenType, groupname.upper()), value)
+
+class Parser:
+    """Recursive-descent formula parser."""
+    def __init__(self, token_iter):
+        self.token_iter = token_iter
+        self.cur = next(self.token_iter)
+    def parse(self):
+        return self.toplevel()
+    def consume(self, type):
+        """If the current token is of the given type, consume and return it.
+
+        Else return None."""
+        cur = self.cur
+        if cur.type == type:
+            try:
+                self.cur = next(self.token_iter)
+            except StopIteration:
+                self.cur = Token(TokenType.EOF, '')
+            return cur
+        return None
+    def expect(self, type):
+        if not self.consume(type):
+            raise ParseError(f"Expected {type}, got {self.cur.type}")
+    def toplevel(self):
+        tm = self.expr()
+        self.expect(TokenType.EOF)
+        return tm
+    def expr(self):
+        return self.sum()
+    def sum(self):
+        tm = self.summand()
+        while True:
+            if self.consume(TokenType.PLUS):
+                tm = ["+", tm, self.summand()]
+            elif self.consume(TokenType.PLUS):
+                tm = ["-", tm, self.summand()]
+            else:
+                break
+        return tm
+    def summand(self):
+        tm = self.factor()
+        while True:
+            if self.consume(TokenType.TIMES):
+                tm = ["*", tm, self.factor()]
+            elif self.consume(TokenType.DIVIDED):
+                tm = ["/", tm, self.factor()]
+            else:
+                break
+        return tm
+    def factor(self):
+        tm = self.consume(TokenType.VALUE)
+        if tm is not None:
+            if self.consume(TokenType.LPAREN):
+                args = self.arglist()
+                self.expect(TokenType.RPAREN)
+                return [tm.value] + args
+            return tm.value
+        if self.consume(TokenType.LPAREN):
+            tm = self.expr()
+            self.expect(TokenType.RPAREN)
+            return tm
+        raise ParseError(f"Expected value or lparen, got {self.cur}")
+    def arglist(self):
+        tm = [self.expr()]
+        while self.consume(TokenType.COMMA):
+            tm = tm + self.arglist()
+        return tm
 
 class ParseError(Exception):
     pass
-
-def _pyast_to_myast(node):
-    if isinstance(node, ast.Num):
-        return node.n
-    elif isinstance(node, ast.BinOp):
-        op_name = OPNAMES_BY_TYPE.get(type(node.op))
-        if op_name is None:
-            raise ParseError("Syntax error")
-        return [
-            op_name,
-            _pyast_to_myast(node.left),
-            _pyast_to_myast(node.right)
-        ]
-    elif isinstance(node, ast.Name):
-        return node.id
-    elif isinstance(node, ast.Call):
-        if not isinstance(node.func, ast.Name):
-            raise ParseError("Invalid function call")
-        if node.keywords:
-            raise ParseError("Syntax error")
-        func = node.func.id
-        return [func] + [_pyast_to_myast(arg) for arg in node.args]
-    else:
-        raise ParseError("Syntax error")
-
-OPNAMES_BY_TYPE = {
-    ast.Add: '+',
-    ast.Sub: '-',
-    ast.Mult: '*',
-    ast.Div: '/'
-}
