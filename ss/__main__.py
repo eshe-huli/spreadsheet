8a90b391006d2ff22486aa104399f87a7a269b37
Ben Kuhn <ben.s.kuhn@gmail.com>
2018-12-26 16:01:12 -0500

    Rename view -> views

--- ss/__main__.py
+++ ss/__main__.py
@@ -2,14 +2,14 @@ import csv
 import curses
 import sys
 
-from ss import engine, view
+from ss import engine, views, models
 
 def read_csv(fname, sheet):
     with open(fname) as f:
         reader = csv.reader(f)
         for row, values in enumerate(reader):
             for col, value in enumerate(values):
-                sheet.set(view.Position(row, col).ref(), value)
+                sheet.set(str(models.Index(row, col)), value)
 
 
 @curses.wrapper
@@ -20,7 +20,7 @@ def main(stdscr):
         if len(sys.argv) > 1:
             fname = sys.argv[1]
             read_csv(fname, sheet)
-        viewer = view.Viewer(sheet, stdscr)
+        viewer = views.Viewer(sheet, stdscr)
         viewer.loop()
     finally:
         curses.noraw()
