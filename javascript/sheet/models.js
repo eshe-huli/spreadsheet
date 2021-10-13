46c8d21d18bc447c15499aeaf1ae398d3beaa1b9
Ruthie Byers <ruthie@wave.com>
2021-10-13 16:02:30 -0400

    Reinstate javascript range models because they're needed for drawing the spreadsheet

--- javascript/sheet/models.js
+++ javascript/sheet/models.js
@@ -174,6 +174,147 @@ class Index {
   }
 }
 
+
+/**
+ * Represents a range of cells like 'A1:B3'.
+ *
+ * `first` and `last` are both *inclusive*, contrary to the usual convention.
+ * This is because:
+ *
+ * 1. ranges are normally referred to by inclusive notation ('A1:B2' includes
+ *    column B and row 2)
+ * 2. ranges are normally constructed by referencing the first and last cells
+ *    that should be included (e.g. if the cursor is on row B2 and the
+ *    selection started on A1, then both A1 and B2 should be included in the
+ *    selection range.)
+ */
+class Range {
+  /**
+   * Construct a range.
+   * You may construct a range from indices that are in the "wrong" order,
+   * and `first` and `last` will be rearranged for you:
+   *
+   * @param {models.Index} pos1
+   * @param {models.Index} pos2
+   *
+   * @example
+   * new Range(new Index(1, 0), new Index(0, 1)).first
+   * // => new Index(0, 0)
+   */
+  constructor(pos1, pos2) {
+    this.first = pos1.min(pos2);
+    this.last = pos1.max(pos2);
+  }
+  /**
+   * Returns true if the range contains the given position.
+   *
+   * @param {models.Index} pos
+   * @return {boolean}
+   *
+   * @example
+   * Range.parse('B2:C3').contains(Index.parse('C3')) // => true
+   * Range.parse('B2:C3').contains(Index.parse('C4')) // => false
+   * Range.parse('B2:C3').contains(Index.parse('D3')) // => false
+   */
+  contains(pos) {
+    return (
+      this.first.row <= pos.row &&
+      pos.row <= this.last.row &&
+      this.first.col <= pos.col &&
+      pos.col <= this.last.col
+    );
+  }
+  /**
+   * Human readable description of the range, like `'A1:B3'`.
+   *
+   * @return {string}
+   *
+   * @example
+   * new Range(Index.parse('A1'), Index.parse('B2')).label
+   * // => "A1:B2"
+   */
+  get label() {
+    return `${this.first.label}:${this.last.label}`;
+  }
+  /**
+   * Number of rows contained in the range.
+   *
+   * @return {number}
+   *
+   * @example
+   * Range.parse('A1:B3').height // => 3
+   */
+  get height() {
+    return this.last.row - this.first.row + 1;
+  }
+  /**
+   * Number of columns contained in the range.
+   *
+   * @return {number}
+   *
+   * @example
+   * Range.parse('A1:B3').width // => 2
+   */
+  get width() {
+    return this.last.col - this.first.col + 1;
+  }
+  /**
+   * Iterator over the indices of the i-th row.
+   *
+   * @return {Generator}
+   *
+   * @example
+   * [...Range.parse('A1:C4').row(1)].map(it => it.label)
+   * // => ['A2', 'B2', 'C2']
+   */
+  *row(i) {
+    if (i >= this.height) {
+      throw new ValueError("Require i < " + this.height);
+    }
+    const start = this.first.add(new Index(i, 0));
+    for (let j = 0; j < this.width; j++) {
+      yield start.add(new Index(0, j));
+    }
+  }
+
+  /**
+   * Iterator over all indices in a range.
+   *
+   * @return {Generator}
+   *
+   * @example
+   * [...Range.parse('A1:B3').indices].map(it => it.label)
+   * // => ['A1', 'B1', 'A2', 'B2', 'A3', 'B3']
+   */
+  get indices() {
+    return function*() {
+      for (let i = 0; i < this.height; i++) {
+        const row = this.row(i);
+        for (const ix of row) {
+          yield ix;
+        }
+      }
+    }.apply(this);
+  }
+  /**
+   * Parse a Range from a string like A1:B3.
+   *
+   * @throws {models.ValueError} if the string is not a valid range.
+   *
+   * @example
+   * Range.parse('A1:B3').label // => 'A1:B3'
+   */
+  static parse(text) {
+    const split = text.split(":");
+    if (split.length != 2) {
+      throw Error(text + " is not a valid range");
+    }
+    const [first, last] = split;
+    return new Range(Index.parse(first), Index.parse(last));
+  }
+}
+
 module.exports = {
-  Index
+  Index,
+  Range
 };
